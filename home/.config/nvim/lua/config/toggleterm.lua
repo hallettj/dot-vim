2bedec99b0011da1e30c0cb53a9999b137337e7b
Jesse Hallett <jesse@sitr.us>
2022-07-05 13:14:13 -0400

    disable toggleterm shading and don't persist size window size changes

--- home/.config/nvim/lua/config/toggleterm.lua
+++ home/.config/nvim/lua/config/toggleterm.lua
@@ -1,11 +1,13 @@
 require("toggleterm").setup {
-  size = function(term)
-    if term.direction == "horizontal" then
-      return 15
-    elseif term.direction == "vertical" then
-      return vim.o.columns * 0.4
-    end
-  end,
-  open_mapping = [[<c-\>]],
-  direction = "vertical"
+    size = function(term)
+        if term.direction == "horizontal" then
+            return 15
+        elseif term.direction == "vertical" then
+            return vim.o.columns * 0.4
+        end
+    end,
+    persist_size = false,
+    open_mapping = [[<c-\>]],
+    direction = "vertical",
+    shade_terminals = false,
 }
