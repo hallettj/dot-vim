03c67aa6ff1bab56f4c6a508aa1f4e53c0e4e288
Jesse Hallett <jesse@sitr.us>
2022-10-13 10:22:54 -0400

    add snippet for Autodocodec object codecs

--- home/.config/nvim/luasnippets/haskell.lua
+++ home/.config/nvim/luasnippets/haskell.lua
@@ -4,7 +4,7 @@ local s = ls.snippet
 -- local isn = ls.indent_snippet_node
 local t = ls.text_node
 local i = ls.insert_node
--- local f = ls.function_node
+local f = ls.function_node
 -- local c = ls.choice_node
 -- local d = ls.dynamic_node
 -- local r = ls.restore_node
@@ -17,9 +17,23 @@ local i = ls.insert_node
 -- local rep = extras.rep
 -- local postfix = require('luasnip.extras.postfix').postfix
 
+-- args is a table, where 1 is the text in Placeholder 1, 2 the text in
+-- placeholder 2, etc.
+local function copy(args)
+  return args[1]
+end
+
 ls.add_snippets('haskell', {
   s({ trig = 'HasCodec', dscr = 'instance for HasCodec' }, {
     t('instance HasCodec '), i(1, 'Type'), t(' where'),
     t({ '', '\tcodec = ' }), i(2, 'undefined'),
+  })
+})
+
+ls.add_snippets('haskell', {
+  s({ trig = 'obj', dscr = 'Autodocodec object codec' }, {
+    t('AC.object "'), f(copy, 1), t('" $ '),
+    t({ '', '\t' }), i(1, 'Type'),
+    t({ '', '\t\t<$> ' }), i(0),
   })
 })
