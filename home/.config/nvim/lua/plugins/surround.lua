1ff9e94f1186abf8419517de48faf854a322b873
Jesse Hallett <jesse@sitr.us>
2023-05-22 15:57:47 -0400

    replace vim-sandwich with nvim-surround

---
+++ home/.config/nvim/lua/plugins/surround.lua
@@ -0,0 +1,9 @@
+return {
+  'kylechui/nvim-surround',
+  version = '*', -- Use for stability; omit to use `main` branch for the latest features
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-treesitter/nvim-treesitter-textobjects',
+  },
+  config = true,
+}
