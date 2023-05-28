4c875f8606887dc2378b1a6465d47d5c1d3eac3e
Jesse Hallett <jesse@sitr.us>
2023-05-28 11:04:21 -0400

    change nvim-surround key maps to avoid conflicts with leap

--- home/.config/nvim/lua/plugins/surround.lua
+++ home/.config/nvim/lua/plugins/surround.lua
@@ -1,3 +1,26 @@
+-- Customize keymaps to avoid conflicts with leap.
+--
+-- The general rule is that if the key ends in "_line", the delimiter pair is
+-- added on new lines. If the key ends in "_cur", the surround is performed
+-- around the current line.
+local keymaps = {
+  insert          = '<C-g>z',
+  insert_line     = '<C-g>Z',
+  normal          = 'gz',
+  normal_cur      = 'gzz',
+  normal_line     = 'gZ',
+  normal_cur_line = 'gZZ',
+  visual          = 'gz',
+  visual_line     = 'gZ',
+  delete          = 'dz',
+  change          = 'cz',
+}
+
+local keys = {}
+for _, v in pairs(keymaps) do
+  table.insert(keys, v)
+end
+
 return {
   'kylechui/nvim-surround',
   version = '*', -- Use for stability; omit to use `main` branch for the latest features
@@ -5,5 +28,8 @@ return {
     'nvim-treesitter/nvim-treesitter',
     'nvim-treesitter/nvim-treesitter-textobjects',
   },
-  config = true,
+  keys = keys,
+  opts = {
+    keymaps = keymaps,
+  },
 }
