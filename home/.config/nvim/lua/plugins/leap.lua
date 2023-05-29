80f7ba73fff380db30607eeeec0f04fa6dbd4014
Jesse Hallett <jesse@sitr.us>
2023-05-29 14:58:18 -0400

    add leap-ast plugin

--- home/.config/nvim/lua/plugins/leap.lua
+++ home/.config/nvim/lua/plugins/leap.lua
@@ -1,13 +1,35 @@
--- `s`/`S` command jumps to occurrence of a pair of characters
 return {
-  'ggandor/leap.nvim',
-  dependencies = { 'tpope/vim-repeat' },
-  config = function()
-    local leap = require('leap')
-    leap.add_default_mappings()
+  -- `s`/`S` command jumps to occurrence of a pair of characters
+  {
+    'ggandor/leap.nvim',
+    dependencies = { 'tpope/vim-repeat' },
+    config = function()
+      local leap = require('leap')
+      leap.add_default_mappings()
 
-    -- I have , and : swapped so I match that configuration in leap
-    leap.opts.special_keys.prev_target = { '<tab>', ':' }
-  end,
+      -- I have , and : swapped so I match that configuration in leap
+      leap.opts.special_keys.prev_target = { '<tab>', ':' }
+    end,
 
+  },
+
+  -- operate on remote pieces of text using leap motions; e.g.
+  -- yar<textobject><leap>
+  {
+    'ggandor/leap-spooky.nvim',
+    dependencies = {
+      'ggandor/leap.nvim',
+      'tpope/vim-repeat'
+    },
+    config = true,
+  },
+
+  -- Leap, but only up the AST
+  {
+    'ggandor/leap-ast.nvim',
+    dependencies = { 'ggandor/leap.nvim' },
+    keys = {
+      { 'gn', function() require 'leap-ast'.leap() end, mode = { 'n', 'x', 'o' }, desc = 'leap to a higher AST node' },
+    },
+  }
 }
--- home/.config/nvim/lua/plugins/treesitter.lua
+++ home/.config/nvim/lua/plugins/treesitter.lua
@@ -9,15 +9,6 @@ return {
           enable = true,          -- false will disable the whole extension
           disable = {},           -- list of languages that will be disabled
         },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = 'gnn',
-            node_incremental = 'grn',
-            scope_incremental = 'grc',
-            node_decremental = 'grm',
-          },
-        },
         indent = { enable = true },
       }
     end,
